f60ec26b2253487c695ebe2855230261fae1c9c8
Mariela Velez <Mariela@Marielas-MacBook-Air.local>
2017-09-14 19:45:46 -0500

    Solution as done  by Cam Kirk along with my own code slightly commented out. Mine works too.

--- server_app.js
+++ server_app.js
@@ -1,25 +1,31 @@
 var http = require('http');
 
 function handleRequestOne(request, response) {
-  response.end("You're amazing!" + req.url);
+  response.end("You're amazing!" + request.url);
 }
 
 function handleRequestTwo(request, response) {
-  response.end("You are not amazing." + req.url);
+  response.end("You are not amazing. You suck." + request.url);
 }
 
-var PORT_ONE = 7000;
-var PORT_TWO = 7500;
+// var PORT_ONE = 7000;
+// var PORT_TWO = 7500;
 
-console.log(typeof server);
-
-var serverOne = http.createServer(handleRequestOne);
-var serverTwo = http.createServer(handleRequestTwo);
-
-serverOne.listen(PORT_ONE, function(){
+var serverOne = http.createServer(handleRequestOne).listen(7000, function(){
   console.log("You're amazing!");
 });
 
-serverTwo.listen(PORT_TWO, function() {
-  console.log("You are not amazing.");
+var serverTwo = http.createServer(handleRequestTwo).listen(7500, function(){
+  console.log("You are not amazing. You suck.");
 });
+
+console.log(typeof serverOne);
+console.log(typeof serverTwo);
+
+// serverOne.listen(PORT_ONE, function(){
+//   console.log("You're amazing!");
+// });
+//
+// serverTwo.listen(PORT_TWO, function() {
+//   console.log("You are not amazing.");
+// });
